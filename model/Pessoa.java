31024ce7456df1e1644c9fee189c4f5944290b59
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-03 16:54:28 -0300

    Iniciando a construção da classe pessoa

---
+++ model/Pessoa.java
@@ -0,0 +1,12 @@
+package model;
+
+public abstract class Pessoa {
+    private int id;
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public String getDados(){
+        return 
+    }
+}
