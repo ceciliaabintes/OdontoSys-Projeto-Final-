315971b01ccbbd3bf07438f9fab49e832e19ed2f
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-03 17:50:27 -0300

    Criação da Classe Paciente

---
+++ model/Paciente.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Paciente extends Pessoa {
+    private String cpf;
+    private String dataNascimento;
+    private String endereco;
+    public Paciente(int id, String nome, String telefone, String email, String cpf,
+        String dataNascimento, String endereco){
+            super(id, nome, telefone, email);
+            this.cpf = cpf;
+            this.dataNascimento = dataNascimento;
+            this.
+    }
+}
