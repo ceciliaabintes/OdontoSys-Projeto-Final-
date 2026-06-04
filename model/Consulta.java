b934bb8f21325165e3f681ef91fba811dce0d3cc
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-03 22:45:25 -0300

    Fazendo o construtor da Classe Consulta

--- model/Consulta.java
+++ model/Consulta.java
@@ -2,7 +2,7 @@ package model;
 
 public class Consulta {
     private int idConsulta;
-    private String dataConsulta;
+    private String dataHora;
     private String tipoConsulta;
     private String Status;
     private String observacoes;
@@ -10,4 +10,11 @@ public class Consulta {
     private Paciente paciente;
     private Dentista dentista;
 
+    public Consulta(int idConsulta, String dataHora, String tipoConsulta, String observacoes){
+        this.idConsulta = idConsulta;
+        this.dataHora = dataHora;
+        this.tipoConsulta = tipoConsulta;
+        this.observacoes = observacoes;
+        this.Status = "Pendente";
+    }
 }
