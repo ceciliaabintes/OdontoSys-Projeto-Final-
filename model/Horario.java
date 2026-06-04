0ad3d349a1d97ae1c31b401970e6b22284ca5218
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-03 22:09:45 -0300

    Implementação da classe e construtor de horário

--- model/Horario.java
+++ model/Horario.java
@@ -1,5 +1,13 @@
 package model;
 
 public class Horario {
+    private String data;
+    private String hora;
+
+    public Horario(String data, String hora){
+        this.data = data;
+        this.hora = hora;
+    }
+
     
 }
