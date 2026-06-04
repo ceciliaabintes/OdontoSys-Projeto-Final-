b66f5deb75850477172a5ff0fe6819ee27ebcfa6
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-03 22:29:28 -0300

    Criaçõa da classe Consulta

---
+++ model/Consulta.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Consulta {
+    private int idConsulta;
+    private String dataConsulta;
+    private String tipoConsulta;
+    private String Status;
+    private String observacoes;
+
+    private Paciente paciente;
+    private Dentista dentista;
+
+}
