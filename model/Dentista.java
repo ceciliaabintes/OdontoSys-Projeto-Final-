accad054a526408b7e9b5d4df568e389aec262c1
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-03 21:56:45 -0300

    Implementação do construtor da Classe Dentista

--- model/Dentista.java
+++ model/Dentista.java
@@ -6,4 +6,14 @@ public class Dentista extends Pessoa {
         private String cro;
         private String especialidade;
         private List<Horario> horarios;
+
+        public Dentista(int id, String nome, String telefone, String email,
+            String cro, String especialidade){
+            super();
+            this.cro = cro;
+            this.especialidade = especialidade;
+            this.horarios = new ArrayList();
+        }
+
+        
 }
