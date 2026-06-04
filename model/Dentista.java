d1d6354efeaccb58279f37d6eb5a96d5af880703
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-03 21:52:18 -0300

    Implementação das classes Dentista e Horario

---
+++ model/Dentista.java
@@ -0,0 +1,9 @@
+package model;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dentista extends Pessoa {
+        private String cro;
+        private String especialidade;
+        private List<Horario> horarios;
+}
