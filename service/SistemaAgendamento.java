d58d76b1f2df01e5f3cd0599ccae889c1e058409
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-10 20:45:35 -0300

    Criação da classe que gerencia os agendamentos

---
+++ service/SistemaAgendamento.java
@@ -0,0 +1,11 @@
+package service;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Consulta;
+import model.Pessoa;
+
+public class SistemaAgendamento {
+    private List<Pessoa> pessoas;
+    private List<Consulta> consultas;
+}
