94e95da2cfe5a3338215564ed4aee568ab2b8176
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-10 20:51:21 -0300

    Implementação dos métodos de gerenciamento de consultas

--- service/SistemaAgendamento.java
+++ service/SistemaAgendamento.java
@@ -8,4 +8,22 @@ import model.Pessoa;
 public class SistemaAgendamento {
     private List<Pessoa> pessoas;
     private List<Consulta> consultas;
+
+    public SistemaAgendamento(){
+        pessoas = new ArrayList<>();
+        consultas = new ArrayList<>();
+    }
+
+    public void iniciar(){
+
+    }
+
+    public void gerenciarConsultas(){
+        
+    }
+    public void listarPessoas(){
+        for(Pessoa pessoa : pessoas){
+            System.out.println(pessoa.getDados());
+        }
+    }
 }
