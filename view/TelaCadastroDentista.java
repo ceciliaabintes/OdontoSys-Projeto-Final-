494102e7d9af3cb7971de311aa3248a3f07fd475
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-15 23:35:19 -0300

    Adicionando campos de entrada para cadastro de dentista na tela de cadastro

--- view/TelaCadastroDentista.java
+++ view/TelaCadastroDentista.java
@@ -6,5 +6,12 @@ import model.Dentista;
 import service.SistemaAgendamento;
 
 public class TelaCadastroDentista extends JFrame{
+    private SistemaAgendamento sistema;
     
+    private JTextField txtId;
+    private JTextField txtNome;
+    private JTextField txtTelefone;
+    private JTextField txtEmail;
+    private JTextField txtCro;
+    private JTextField txtEspecialidade;
 }
