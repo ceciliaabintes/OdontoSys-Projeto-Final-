78a05c88869e17a77bdfa096947df33a2bebe1ba
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-15 21:54:28 -0300

    Criando a tela de Cadastro de Paciente

---
+++ view/TelaCadastroPaciente.java
@@ -0,0 +1,11 @@
+package view;
+import javax.swing.*;
+
+import model.Paciente;
+import service.SistemaAgendamento;
+
+public class TelaCadastroPaciente extends JFrame {
+    private SistemaAgendamento sistema;
+
+    private JTextField txtI
+}
