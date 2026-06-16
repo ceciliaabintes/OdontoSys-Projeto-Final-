d1dd01712bf460089dcacf014eb4e3307c4d4236
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-16 20:57:12 -0300

    Adicionando a tabela para exibir consultas agendadas na tela de listagem

--- view/TelaListarConsultas.java
+++ view/TelaListarConsultas.java
@@ -1,6 +1,7 @@
 package view;
 import javax.swing.*;
 import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.DefaultTableModel;
 
 import model.Consulta;
 import service.SistemaAgendamento;
@@ -17,5 +18,22 @@ public class TelaListarConsultas extends JFrame {
         setTitle("Consultas Agendadas");
         setSize(800,400);
         setLocationRelativeTo(null);
+
+        modelo = new DefaultTableModel();
+
+        modelo.addColumn("ID");
+        modelo.addColumn("Paciente");
+        modelo.addColumn("Dentista");
+        modelo.addColumn("Tipo");
+        modelo.addColumn("Status");
+
+        table = new JTable(modelo);
+        carregaConsultas();
+
+        add(new JScrollPane(table));
+        setVisible(true);
+    }
+    private void carregaConsultas(){
+        
     }
 }
