5e5b77a2f56e239ad972753336c4c21aa6d3bde0
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-16 14:34:37 -0300

    Iniciando a criação da tela de agendamento de consultas

---
+++ view/TelaAgendarConsulta.java
@@ -0,0 +1,13 @@
+package view;
+import javax.swing.*;
+import java.awt.*;
+
+import model.Consulta;
+import model.Dentista;
+import model.Horario;
+import model.Paciente;
+import service.SistemaAgendamento;
+
+public class TelaAgendarConsulta extends JFrame {
+    
+}
