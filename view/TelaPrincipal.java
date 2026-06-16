586596d2fc8590f94becacedf252c834a0ecfd9f
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-15 21:01:03 -0300

    Iniciando A criação de telas

---
+++ view/TelaPrincipal.java
@@ -0,0 +1,28 @@
+package view;
+import javax.swing.*;
+import java.awt.*;
+public class TelaPrincipal extends JFrame{
+    private JButton btnPaciente;
+    private JButton btnDentista;
+    private JButton btnHorario;
+    private JButton btnConsulta;
+    private JButton btnListar;
+    private JButton btnSair;
+
+    public TelaPrincipal(){
+        setTitle("OdontoSys");
+        setSize(700, 700);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLocation(null);
+
+        setLayout(new GridLayout (6,1,10,10));
+
+        btnPaciente = new JButton("Cadastrar Paciente");
+        btnDentista = new JButton("Cadastrar Dentista");
+        btnHorario = new JButton("Cadastrar Horario");
+        btnConsulta = new JButton("Agendar Consulta");
+        btnListar = new JButton("Cadastrar Paciente");
+    }
+}
