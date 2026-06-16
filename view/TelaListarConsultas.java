ab86b14f12fcc11bf8480a7353e5ebb34856a7ff
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-16 18:51:39 -0300

    Adicionando a classe ListarConsultas para exibir consultas agendadas

---
+++ view/TelaListarConsultas.java
@@ -0,0 +1,10 @@
+package view;
+import javax.swing.*;
+import javax.swing.table.DefaultTableColumnModel;
+
+import model.Consulta;
+import service.SistemaAgendamento;
+
+public class TelaListarConsultas extends JFrame {
+    private SistemaAgendamento sistema;
+}
