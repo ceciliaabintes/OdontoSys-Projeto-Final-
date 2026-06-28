0135d71014cb399b040fe229b5bf98ec2158682e
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-28 18:56:09 -0300

    Criando a tela de listagem de pacientes

---
+++ view/TelaListarPacientes.java
@@ -0,0 +1,38 @@
+package view;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import model.Paciente;
+import service.SistemaAgendamento;
+public class TelaListarPacientes extends JFrame{
+    
+    private SistemaAgendamento sistema;
+
+    private JTable tabela;
+    private DefaultTableModel modelo;
+
+     public TelaListarPacientes(SistemaAgendamento sistema){
+
+        this.sistema = sistema;
+
+        setTitle("Pacientes Cadastrados");
+        setSize(800,400);
+        setLocationRelativeTo(null);
+
+        modelo = new DefaultTableModel();
+
+        modelo.addColumn("ID");
+        modelo.addColumn("Nome");
+        modelo.addColumn("CPF");
+        modelo.addColumn("Telefone");
+        modelo.addColumn("Email");
+
+        tabela = new JTable(modelo);
+
+        carregarPacientes();
+
+        add(new JScrollPane(tabela));
+
+        setVisible(true);
+    }
+}
