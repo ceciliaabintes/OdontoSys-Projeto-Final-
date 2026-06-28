348948c949053abb6a54266e74dc4eec49be55c3
ceciliaabintes_ <142744298+ceciliaabintes@users.noreply.github.com>
2026-06-28 18:53:59 -0300

    Criando uma tela para lsitagem de pacientes

---
+++ view/TelaLiatarPacientes.java
@@ -0,0 +1,14 @@
+package view;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import model.Paciente;
+import service.SistemaAgendamento;
+public class TelaLiatarPacientes extends JFrame{
+    
+    private SistemaAgendamento sistema;
+
+    private JTable tabela;
+    private DefaultTableModel modelo;
+
+}
